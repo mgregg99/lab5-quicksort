a6b4573a328955c57177ea76414e301fd4a92e8e
Matthew <mattgregg57@gmail.com>
2021-10-19 08:30:39 -0600

    main2 help

--- main2.cpp
+++ main2.cpp
@@ -1,42 +1,74 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int *ptr;
-int arraysize;
+// int *ptr;
+// int arraysize;
 
-void redo(){
-    int neww[99];
-    arraysize = 99;
+// void redo(){
+//     int neww[9];
+//     arraysize = 9;
 
+//     ptr = &neww[0];
+   
 
-    ptr = neww;
+//     for (int i = 0; i < arraysize; i++){
+//         *(ptr + i) = i + 10;
+//     }
+     
+// }
 
-}
-
-int main(){
-    int apple[9];
-    arraysize = 9;
+// int main(){
+//     int apple[9];
+//     arraysize = 9;
 
     
 
-    for(int i = 0; i < arraysize -1; i++){
-        apple[i] = i + 1;
+//     for(int i = 0; i < arraysize; i++){
+//         apple[i] = i + 1;
+//     }
+//     ptr = apple;
+
+//     for(int i = 0; i < arraysize; i++){
+//         apple[i] = i + 1;
+//         cout << *(ptr + i) << endl;
+//     }
+
+//     redo();
+
+//     cout << "-----------------" << endl;
+
+//     for(int i = 0; i < arraysize; i++){
+//         apple[i] = i + 1;
+//         cout << *(ptr + i) << endl;
+//     }
+
+//     return 1;
+// }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float arr[5];
+    
+   // Insert data using pointer notation
+    cout << "Enter 5 numbers: ";
+    for (int i = 0; i < 5; ++i) {
+
+        // store input number in arr[i]
+        cin >> 
+        *(arr + i) ;
+
     }
-    ptr = apple;
 
-    for(int i = 0; i < arraysize -1; i++){
-        apple[i] = i + 1;
-        cout << *(ptr + i) << endl;
+    // Display data using pointer notation
+    cout << "Displaying data: " << endl;
+    for (int i = 0; i < 5; ++i) {
+
+        // display value of arr[i]
+        cout << *(arr + i) << endl ;
+
     }
 
-    redo();
-
-
-
-    for(int i = 0; i < arraysize -1; i++){
-        apple[i] = i + 1;
-        cout << *(ptr + i) << endl;
-    }
-
-    return 1;
+    return 0;
 }
